0d8b438c1475fe7fdb1827a4a2fd588201ddc80c
oh344225 <nov.oh@icloud.com>
2017-04-06 19:23:30 +0900

    exif+ test

--- pulshot01verwatch/ViewController.swift
+++ pulshot01verwatch/ViewController.swift
@@ -10,8 +10,24 @@ import HealthKit
 
 import AVFoundation
 
+//exif情報
+import ImageIO
+import Photos
+import MobileCoreServices
+
+import AssetsLibrary
 
 class ViewController: UIViewController, UITextFieldDelegate ,AVCapturePhotoCaptureDelegate{
+
+	//各インスタンス作成
+	var myHealthStore : HKHealthStore = HKHealthStore()
+	
+	var myReadHeartRateField: UITextField!
+	//var myReadButton: UIButton!
+
+	//bpm
+	//var bpm
+	
 	
 	//storyboard imageview
 	@IBOutlet weak var imageView: UIImageView!
@@ -22,6 +38,7 @@ class ViewController: UIViewController, UITextFieldDelegate ,AVCapturePhotoCaptu
 	
 	
 	@IBAction func takePhoto(_ sender: Any) {
+		//takephoto設定
 		let settingsForMonitoring = AVCapturePhotoSettings()
 		settingsForMonitoring.flashMode = .auto
 		settingsForMonitoring.isAutoStillImageStabilizationEnabled = true
@@ -42,9 +59,13 @@ class ViewController: UIViewController, UITextFieldDelegate ,AVCapturePhotoCaptu
 		
 		let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
 		do{
+			
 			let input = try AVCaptureDeviceInput(device: device)
+			//入力
 			if (captureSession.canAddInput(input)){
 				captureSession.addInput(input)
+				
+				//出力
 				if (captureSession.canAddOutput(stillImgaeOutput)){
 					captureSession.addOutput(stillImgaeOutput)
 					captureSession.startRunning()
@@ -60,21 +81,163 @@ class ViewController: UIViewController, UITextFieldDelegate ,AVCapturePhotoCaptu
 		}
 		
 	}
+
+	override func didReceiveMemoryWarning() {
+		
+			super.didReceiveMemoryWarning()
+			// Dispose of any resources that can be recreated.
+	
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+	
+		super.viewDidAppear(animated)
+		//Healthstoreから許可申請
+		requestAuthorization()
+		
+	}
+	
+	
+	
+	//healthkitへのアクセス申請関数
+	private func requestAuthorization(){
+		
+		//読み込みを許可する型
+		let type = Set(arrayLiteral: HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!)
+		
+		//Healthstoreアクセス承認を行う
+		myHealthStore.requestAuthorization(toShare: type, read: type, completion: {(sucess,error)in
+			if let e = error{
+				print("Error:\(e.localizedDescription)")
+			}
+			print(sucess ? "Sucess" : "Failture")
+			
+		})
+	}
+	
+	//心拍データ読み出し関数
+	private func readData(){
+		var error : NSError!
+		
+		//取得したいデータのタイプを生成
+		let typeOfHeartRate = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)
+		
+		let calender = Calendar.init(identifier: Calendar.Identifier.gregorian)
+		let now = Date()
+		
+		let startDate = calender.startOfDay(for: now)
+		let endDate = calender.date(byAdding: Calendar.Component.day,value: 1 ,to: startDate)
+		
+		let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+		
+		//データ取得時に登録された時間でソートする
+		let mySortDescription = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+		//query生成
+		let myquery = HKSampleQuery(sampleType: typeOfHeartRate!, predicate: predicate, limit: 1, sortDescriptors: [mySortDescription])
+		{ (sampleQuery, results, error) -> Void in
+			
+			
+			//一番最近に登録されたデータ取得
+			guard let myRecentSample = results!.first as? HKQuantitySample else{
+				print("error")
+				self.myReadHeartRateField.text = "Data is not found"
+				return
+			}
+			
+			//取得したサンプルを単位に合わせる
+			DispatchQueue.main.async {
+				
+				let count:HKUnit = HKUnit.count()
+				let minute: HKUnit = HKUnit.minute()
+				let countPerMinute:HKUnit = count.unitMultiplied(by: minute.reciprocal())
+				
+				let bpm = myRecentSample.quantity.doubleValue(for: countPerMinute)
+				
+				//var pulsepersec = myRecentSample.quantity
+				//var pulse = pulsepersec * 60
+				print(bpm)
+				//self.myReadHeartRateField.text = "\(bpm)"
+				
+			}
+		}
+		
+		//query発行
+		self.myHealthStore.execute(myquery)
+	}
+	
 	
 	func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?,previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings : AVCaptureResolvedPhotoSettings, bracketSettings : AVCaptureBracketedStillImageSettings?, error: Error?){
 		
+		readData()
+		
+		
 		if let photoSampleBuffer = photoSampleBuffer{
-			//jpeg形式に保存
+		
+			
+			//jpeg形式で取得
 			let photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer:previewPhotoSampleBuffer)
 			
+			//let image = UIImage(data: photoData!)
 			
-			let image = UIImage(data: photoData!)
-			//フォトライブラリに保存
-			UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+			
+			let wi: UIImage = UIImage(data: photoData!)!
+			
+			let cg : CGImage = wi.cgImage!;
+			
+			let image = UIImage(cgImage: cg, scale: wi.scale, orientation: .up)
+			
+			
+			//exifデータ作成
+			let exif = NSMutableDictionary()
+			//Exifにコメント情報をセットする
+			//let value = myRecentSample.quantity.doubleValue(for: countPerMinute)
+			
+			/*
+			exif.setObject("photoshot",forKey: kCGImagePropertyPNGTitle as CFString as! NSCopying)
+			exif.setObject(comment, forKey: kCGImagePropertyExifUserComment as NSString)
+			*/
+			//exif.setObject("photoshot", forKey: kCGImagePropertyPNGTitle as CFString)
+			exif[(kCGImagePropertyExifUserComment as CFString)] = "hoge"
+			
+			
+			//静止画metadata作成
+			let metadata = NSMutableDictionary()
+			metadata.setObject(exif, forKey: kCGImagePropertyExifDictionary as! NSCopying )
+			
+			//フォト保存
+			//メタデータ保存のためphotoframework 使用
+			let tmpName = ProcessInfo.processInfo.globallyUniqueString
+			let tmpUrl = NSURL.fileURL(withPath: NSTemporaryDirectory() + tmpName + "jpg")
+			//print(tmpUrl)
+			
+			
+			if let dest = CGImageDestinationCreateWithURL(tmpUrl as CFURL, kUTTypeJPEG, 1, nil){
+				
+				CGImageDestinationAddImage(dest, cg, (metadata as CFDictionary))
+				CGImageDestinationFinalize(dest)
+				print(dest)
+				
+				//print(metadata)
+				//保存処理
+				let library = PHPhotoLibrary.shared
+				library().performChanges({ PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: tmpUrl) },completionHandler: { (ok, err) in print(ok, err)
+					//let _ = try? FileManager.default.removeItem(at:tmpUrl)
+				})
+				
+
+			}
+
+			
+			
+			
+			//フォトライブラリ保存
+			//UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+			
 			
 		}
 		
 	}
+	
 	
 	
 	
